29e5ab10e6de049468e047d6a7e611ad1b5f4dcf
ctooley21 <christian.tooley@uky.edu>
2017-11-22 16:00:25 -0500

    Ensure /ci doesn't let players keep class effects since no ArmorChangeEvent is fired

--- src/main/java/com/desiremc/essentials/commands/ClearInventoryCommand.java
+++ src/main/java/com/desiremc/essentials/commands/ClearInventoryCommand.java
@@ -1,6 +1,9 @@
 package com.desiremc.essentials.commands;
 
 import com.desiremc.core.session.Rank;
+import com.desiremc.hcf.listener.classes.ClassListener;
+import com.desiremc.hcf.session.HCFSession;
+import com.desiremc.hcf.session.HCFSessionHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -15,6 +18,10 @@ public class ClearInventoryCommand extends PlayerChangeCommand
     @Override
     public Object[] applyChanges(Player p, Object[] args)
     {
+
+        HCFSession session = HCFSessionHandler.getHCFSession(p.getUniqueId());
+        session.setPvpClass(null);
+        ClassListener.updateClass(p, p.getInventory().getHelmet());
 
         p.getInventory().setContents(new ItemStack[p.getInventory().getContents().length]);
         p.getInventory().setArmorContents(new ItemStack[p.getInventory().getArmorContents().length]);
