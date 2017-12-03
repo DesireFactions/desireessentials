27137e2860b35d0539a472f6d0de28c7df9b91ee
Michael <Michael@DESKTOP-78LM4BC>
2017-12-03 14:20:37 -0600

    Stop always displaying world, also use parsers properly.

--- src/main/java/com/desiremc/essentials/commands/TeleportPositionCommand.java
+++ src/main/java/com/desiremc/essentials/commands/TeleportPositionCommand.java
@@ -1,31 +1,29 @@
 package com.desiremc.essentials.commands;
 
-import com.desiremc.core.api.command.ValidCommand;
-import com.desiremc.core.parsers.IntegerParser;
-import com.desiremc.core.parsers.StringParser;
-import com.desiremc.core.session.Rank;
-import com.desiremc.core.validators.BukkitWorldValidator;
-import com.desiremc.core.validators.PlayerValidator;
-import com.desiremc.essentials.DesireEssentials;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.desiremc.core.api.command.ValidCommand;
+import com.desiremc.core.parsers.DoubleParser;
+import com.desiremc.core.parsers.WorldParser;
+import com.desiremc.core.session.Rank;
+import com.desiremc.core.validators.PlayerValidator;
+import com.desiremc.essentials.DesireEssentials;
+
 public class TeleportPositionCommand extends ValidCommand
 {
     public TeleportPositionCommand()
     {
-        super("teleportposition", "Teleport to another position", Rank.JRMOD, ARITY_OPTIONAL, new String[] {"x", "y", "z", "world"}, "tppos");
+        super("teleportposition", "Teleport to another position", Rank.JRMOD, ARITY_OPTIONAL, new String[] { "x", "y", "z", "world" }, "tppos");
 
-        addParser(new IntegerParser(), "x");
-        addParser(new IntegerParser(), "y");
-        addParser(new IntegerParser(), "z");
-        addParser(new StringParser(), "world");
+        addParser(new DoubleParser(), "x");
+        addParser(new DoubleParser(), "y");
+        addParser(new DoubleParser(), "z");
+        addParser(new WorldParser(), "world");
 
         addValidator(new PlayerValidator());
-        addValidator(new BukkitWorldValidator(), "world");
     }
 
     @Override
@@ -33,25 +31,31 @@ public class TeleportPositionCommand extends ValidCommand
     {
         Player player = (Player) sender;
 
-        int x = (int) args[0];
-        int y = (int) args[1];
-        int z = (int) args[2];
+        double x = (double) args[0];
+        double y = (double) args[1];
+        double z = (double) args[2];
 
-        Location loc;
+        Location loc = player.getLocation().clone();
+
+        loc.setX(x);
+        loc.setY(y);
+        loc.setZ(z);
 
         if (args.length > 3)
         {
-            World world = Bukkit.getWorld((String) args[3]);
-            loc = new Location(world, x, y, z);
-        }
-        else
-        {
-            loc = new Location(player.getWorld(), x, y, z);
+            loc.setWorld((World) args[3]);
         }
 
         player.teleport(loc);
 
-        DesireEssentials.getLangHandler().sendRenderMessage(player, "teleport-position.self", "{x}", loc.getX(), "{y}", loc.getY(), "{z}", loc.getZ(), "{world}", loc.getWorld().getName());
+        if (args.length > 3)
+        {
+            DesireEssentials.getLangHandler().sendRenderMessage(player, "teleport_position.world", "{x}", loc.getX(), "{y}", loc.getY(), "{z}", loc.getZ(), "{world}", loc.getWorld().getName());
+        }
+        else
+        {
+            DesireEssentials.getLangHandler().sendRenderMessage(player, "teleport_position.no_world", "{x}", loc.getX(), "{y}", loc.getY(), "{z}", loc.getZ());
+        }
     }
 
 }
