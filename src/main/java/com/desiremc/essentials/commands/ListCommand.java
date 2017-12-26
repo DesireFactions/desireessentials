4438e5de6673c4e1287ff24064891c6474c778bd
ctooley21 <christian.tooley@uky.edu>
2017-12-26 10:18:27 -0600

    Not supposed to do that.

--- src/main/java/com/desiremc/essentials/commands/ListCommand.java
+++ src/main/java/com/desiremc/essentials/commands/ListCommand.java
@@ -1,14 +1,15 @@
 package com.desiremc.essentials.commands;
 
+import java.util.List;
+
+import org.bukkit.Bukkit;
+
 import com.desiremc.core.api.newcommands.CommandArgument;
 import com.desiremc.core.api.newcommands.ValidCommand;
 import com.desiremc.core.session.Rank;
 import com.desiremc.core.session.Session;
 import com.desiremc.core.session.SessionHandler;
 import com.desiremc.essentials.DesireEssentials;
-import org.bukkit.Bukkit;
-
-import java.util.List;
 
 public class ListCommand extends ValidCommand
 {
@@ -33,7 +34,9 @@ public class ListCommand extends ValidCommand
 
         for (Session s : SessionHandler.getOnlineStaff())
         {
-            sb.append("§e" + s.getName() + ", ");
+            sb.append("§e");
+            sb.append(s.getName());
+            sb.append(", ");
         }
         if (sb.length() > 0)
         {
